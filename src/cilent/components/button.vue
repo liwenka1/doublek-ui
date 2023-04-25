6612b42048ac04e11ef4756c22b801e34970eaa4
liwenkai <2020583117@qq.com>
2023-04-25 23:53:00 +0800

    button样式添加disable round circle

--- src/cilent/components/button.vue
+++ src/cilent/components/button.vue
@@ -1,16 +1,53 @@
 <template>
-  <k-button>click me!
-  </k-button>
-  <k-button type="primary">click me!
-  </k-button>
-  <k-button type="success">click me!
-  </k-button>
-  <k-button type="info">click me!
-  </k-button>
-  <k-button type="danger">click me!
-  </k-button>
-  <k-button type="warning">click me!
-  </k-button>
+  <k-button>default</k-button>
+  <k-button type="primary">primary </k-button>
+  <k-button type="success">success</k-button>
+  <k-button type="info">info</k-button>
+  <k-button type="danger">danger </k-button>
+  <k-button type="warning">warning</k-button>
+  <br />
+  <k-button plain>default</k-button>
+  <k-button plain type="primary">primary </k-button>
+  <k-button plain type="success">success</k-button>
+  <k-button plain type="info">info</k-button>
+  <k-button plain type="danger">danger </k-button>
+  <k-button plain type="warning">warning</k-button>
+  <br />
+  <k-button disabled>default</k-button>
+  <k-button disabled type="primary">primary </k-button>
+  <k-button disabled type="success">success</k-button>
+  <k-button disabled type="info">info</k-button>
+  <k-button disabled type="danger">danger </k-button>
+  <k-button disabled type="warning">warning</k-button>
+  <br />
+  <k-button disabled plain>default</k-button>
+  <k-button disabled plain type="primary">primary </k-button>
+  <k-button disabled plain type="success">success</k-button>
+  <k-button disabled plain type="info">info</k-button>
+  <k-button disabled plain type="danger">danger </k-button>
+  <k-button disabled plain type="warning">warning</k-button>
+  <br />
+  <k-button round>default</k-button>
+  <k-button round type="primary">primary </k-button>
+  <k-button round type="success">success</k-button>
+  <k-button round type="info">info</k-button>
+  <k-button round type="danger">danger </k-button>
+  <k-button round type="warning">warning</k-button>
+  <br />
+  <k-button circle></k-button>
+  <k-button circle type="primary"></k-button>
+  <k-button circle type="success"></k-button>
+  <k-button circle type="info"></k-button>
+  <k-button circle type="danger"></k-button>
+  <k-button circle type="warning"></k-button>
+  <br />
+  <k-button size="large">large </k-button>
+  <k-button>default </k-button>
+  <k-button size="small">small </k-button>
+  <br />
+  <k-button circle size="large"> </k-button>
+  <k-button circle> </k-button>
+  <k-button circle size="small"> </k-button>
 </template>
 
 <script lang="ts" setup>
