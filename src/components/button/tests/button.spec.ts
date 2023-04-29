d203c28537108cdb4f84200a86fda0472cb6c904
liwenkai <2020583117@qq.com>
2023-04-29 12:23:05 +0800

    button icon 单元测试添加

--- src/components/button/tests/button.spec.ts
+++ src/components/button/tests/button.spec.ts
@@ -11,12 +11,68 @@ describe("Button:Test", () => {
     });
     expect(wrapper.text()).toContain("Hello World");
   });
-  it('should have class', () => {
+  it("should have type", () => {
     const wrapper = mount(Button, {
       props: {
-        type: 'primary'
-      }
+        type: "primary",
+      },
     });
-    expect(wrapper.classes()).toContain('k-button--primary');
+    expect(wrapper.classes()).toContain("k-button--primary");
+  });
+  it("should have size", () => {
+    const wrapper = mount(Button, {
+      props: {
+        type: "large",
+      },
+    });
+    expect(wrapper.classes()).toContain("k-button--large");
+  });
+  it("should have disable", () => {
+    const wrapper = mount(Button, {
+      props: {
+        disabled: true,
+      },
+    });
+    expect(wrapper.classes()).toContain("is-disabled");
+  });
+  it("should have loading", () => {
+    const wrapper = mount(Button, {
+      props: {
+        loading: true,
+      },
+    });
+    expect(wrapper.classes()).toContain("is-loading");
+  });
+  it("should have round", () => {
+    const wrapper = mount(Button, {
+      props: {
+        round: true,
+      },
+    });
+    expect(wrapper.classes()).toContain("is-round");
+  });
+  it("should have plain", () => {
+    const wrapper = mount(Button, {
+      props: {
+        plain: true,
+      },
+    });
+    expect(wrapper.classes()).toContain("is-plain");
+  });
+  it("should have circle", () => {
+    const wrapper = mount(Button, {
+      props: {
+        circle: true,
+      },
+    });
+    expect(wrapper.classes()).toContain("is-circle");
+  });
+  it("should have link", () => {
+    const wrapper = mount(Button, {
+      props: {
+        link: true,
+      },
+    });
+    expect(wrapper.classes()).toContain("is-link");
   });
 });
---
+++ src/components/icon/teste/icon.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import Icon from "../src/icon.vue";
+
+describe("Icon:Test", () => {
+  it("should have name", () => {
+    const wrapper = mount(Icon, {
+      props: {
+        name: "cart-add-fill",
+      },
+    });
+    expect(wrapper.classes()).toContain("k-icon-cart-add-fill");
+  });
+});
