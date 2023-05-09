2ef58e79525eebd2360da8b36dccaf360ec8a7bd
liwenkai <2020583117@qq.com>
2023-05-09 22:15:52 +0800

    fix: alert组件

---
+++ src/components/alert/src/alert.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default { name: 'KAlert' }
+</script>
+
+<template>
+    <transition name="k-alert-fade">
+        <div>123</div>
+    </transition>
+</template>
+
+<script setup>
+import '../styles/index'
+import {} from 'vue'
+    
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
