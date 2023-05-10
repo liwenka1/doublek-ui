73b73f0d40c8eef45e8ec727dff4a3b282116209
liwenkai <2020583117@qq.com>
2023-05-10 14:00:53 +0800

    fix: alert格式化

--- src/components/alert/src/alert.vue
+++ src/components/alert/src/alert.vue
@@ -3,17 +3,14 @@ export default { name: 'KAlert' }
 </script>
 
 <template>
-    <transition name="k-alert-fade">
-        <div>123</div>
-    </transition>
+  <transition name="k-alert-fade">
+    <div>123</div>  
+  </transition>
 </template>
 
 <script setup>
 import '../styles/index'
 import {} from 'vue'
-    
 </script>
 
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
